a5f03f2c3b15cecb9c7a654e05ab7bb48cd43f12
Tinnakorn Choompee <tinnakornchoompee@gmail.com>
2019-11-08 14:11:12 +0700

    Group

--- src/app/group/group.component.ts
+++ src/app/group/group.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-group',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GroupComponent implements OnInit {
 
+  userForm = new FormGroup({
+    firstName : new FormControl('', Validators.required),
+    lastName  : new FormControl(''),
+    age       : new FormControl(''),
+    email     : new FormControl(''),
+    address   : new FormGroup({
+      city : new FormControl(''),
+      zip  : new FormControl('')
+    })
+  });
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    console.warn(this.userForm.value);
+    console.log(this.userForm.controls.firstName.value);
+    console.log(this.userForm.controls.address.value.city);
+    console.log(this.userForm.get('firstName').value);
+    console.log(this.userForm.get(['address', 'city']).value);
+    console.log(this.userForm.get('address').get('city').value);
+  }
+
+  updateModel() {
+    this.userForm.patchValue({
+      firstName: 'tinnakorn',
+      address: {
+        city: 'cm'
+      }
+    });
   }
 
 }
